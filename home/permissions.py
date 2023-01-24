8dd93ed98c1174f45a24d83dc10a1f756741f9d0
Pallav Rai <pallavrai8953@gmail.com>
2023-01-24 23:28:09 +0530

    edited permssions

--- home/permissions.py
+++ home/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class IsownerPermission(permissions.DjangoModelPermissions):
     def has_object_permission(self, request, view, obj):
         
-        if obj.owner_id == request.user:
+        if obj.owner_id == request.user.id:
             return True
 
         return False
