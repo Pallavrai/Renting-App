d9222416fff98ad6d04a8b8d96a28045947b29c9
Pallav Rai <pallavrai8953@gmail.com>
2023-01-26 13:43:41 +0530

    fixed Fix Owner updation problem #1

--- home/permissions.py
+++ home/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework import permissions
 
-class IsownerPermission(permissions.DjangoModelPermissions):
+class IsownerPermission(permissions.BasePermission):
+    message="Sorry your are not a owner."
+    
     def has_object_permission(self, request, view, obj):
         
-        if obj.owner_id == request.user.id:
+        if obj.owner_id == request.user:
             return True
 
         return False
